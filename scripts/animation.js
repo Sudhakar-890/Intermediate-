36c79e77ebc96b8671fed783effb6298c270a784
sudhakar-890 <sudhakarsuresh4321@gmail.com>
2026-02-28 21:21:09 +0530

    ui stage 2 completed

--- scripts/animation.js
+++ scripts/animation.js
@@ -7,8 +7,8 @@ const search = document.querySelector('input[type=search]');
 searchIcon.addEventListener('click',toggleSearch);
 
 function toggleSearch(){
-    text.style.animation = 'rotateTask 0.5s';
-    search.style.animation = 'rotateSearch 0.5s';
+    text.style.animation = 'rotateTask 0.5s ease-in';
+    search.style.animation = 'rotateSearch 0.5s ease-in';
     text.classList.toggle('task-hide');
     search.classList.toggle('search-active');
 }
