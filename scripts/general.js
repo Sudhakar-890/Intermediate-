cfa5e8b16a2c675b9a204351892fdf8decf817ca
Sudhakar-890 <sudhakarsuresh4321@gmail.com>
2026-03-05 00:24:50 +0530

    update

--- scripts/general.js
+++ scripts/general.js
@@ -1,17 +1,14 @@
 /* pile animation */
 
+const file = document.title;
 const piles = document.querySelectorAll('footer a');
-piles.forEach((pile,index)=>{
-    pile.addEventListener('click',()=>{
-        current = index;
-        localStorage.setItem('c_pile',JSON.stringify(index));
-    });
-})
+changeTab()
 
-current = JSON.parse(localStorage.getItem('c_pile')) || 0;
-changePileColor(current);
-
-function changePileColor(current){
-    const temp = piles[current];
-    temp.style.filter='grayscale(0)';
+function changeTab(){
+    if(file=='Smart Task Pro'){
+        piles[0].style.filter='grayscale(0)';
+    }
+    else if(file=='Task List'){
+        piles[1].style.filter='grayscale(0)';
+    }
 }
