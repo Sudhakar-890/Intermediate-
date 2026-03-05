d8dc46de79c06e10857ffecd07197b6d2a581100
sudhakar-890 <sudhakarsuresh4321@gmail.com>
2026-03-05 22:07:50 +0530

    ppriority tabs are set

--- scripts/task-list/task-view-animate.js
+++ scripts/task-list/task-view-animate.js
@@ -13,10 +13,22 @@ function changePriorTab(Tab){
         }
         else{
             prior.classList.add('curPriorTab');
+            scrollToTab(Tab)
         }
     });
     
 }
+
+function scrollToTab(Tab){
+    const tabs = document.querySelectorAll('.priorTabs');
+    console.log(priors[Tab])
+    tabs[Tab].scrollIntoView({
+        behavior:'smooth',
+        block:'center',
+        inline:'center'
+    })
+}
+
 
 let currentBtn = 0;
 const filterBtns = document.querySelectorAll('.filterBtn');
@@ -36,13 +48,17 @@ function changeFilterBtn(btn){
     });
 }
 
-/*scroll into views
+/* searchbar change */
 
-const priorTabs = document.querySelectorAll('priorTabs');
-priorTabs.scrollIntoView({
-    behavior:'smooth',
-    inline:'center',
-    block : 'center'
-})
+const priorityBox = document.querySelector('#priorityBox');
+const searchIcon = document.querySelector('#searchIcon');
+const search = document.querySelector('input[type=search]');
+searchIcon.addEventListener('click',toggleSearch);
 
-*/
+function toggleSearch(){
+    priorityBox.style.animation = 'rotateTask 0.3s ease-out';
+    search.style.animation = 'rotateSearch 0.3s ease-out';
+    priorityBox.classList.toggle('task-hide');
+    search.classList.toggle('search-active');
+}
+
